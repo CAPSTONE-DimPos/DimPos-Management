9657f522cc932d056ff55330ed76500c871daad9
imnotfine20 <138184390+HoangNguyen77@users.noreply.github.com>
2025-07-17 20:15:14 +0700

    feat: create product with modifier group and variants

--- src/schema/product-variant.schema.ts
+++ src/schema/product-variant.schema.ts
@@ -18,8 +18,8 @@ export const CreateProductVariantSchema = z.object({
     name: z.string({ message: "Tên của biến thể sản phẩm không được bỏ trống" }).min(1, { message: "Tên của biến thể sản phẩm phải có ít nhất 1 ký tự" }).max(200, { message: "Tên của biến thể sản phẩm không được vượt quá 200 ký tự" }),
     description: z.string().nullable(),
     sku: z.string({message: "Mã SKU của biến thể sản phẩm không được bỏ trống"}).min(1, { message: "Mã SKU của biến thể sản phẩm có ít nhất 1 ký tự" }).max(100, { message: "Mã SKU của biến thể sản phẩm không được vượt quá 100 ký tự" }).optional(),
-    brandPrice: z.number().min(0, { message: "Giá brand của biến thể sản phẩm không được bỏ trống" }),
-    size: z.string().optional(),
+    brandPrice: z.number({message: "Giá của biến thể sản phẩm không được bỏ trống"}).min(1, { message: "Giá của biến thể sản phẩm phải lớn hơn 0" }),
+    size: z.string({message: "Kích thước của biến thể sản phẩm không được bỏ trống"}).min(1, { message: "Kích thước của biến thể sản phẩm không được bỏ trống" }).max(50, { message: "Kích thước của biến thể sản phẩm không được vượt quá 50 ký tự" }),
     displayOrder: z.number().int().optional(),
 });
 
